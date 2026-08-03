6157c0dc8583cc264ac91579c3b69da2b3e59bde
Alex Turnaev <turnev0505@gmail.com>
2026-08-03 21:50:24 +0400

    Stylelint fix3

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,21 +2,21 @@ export default {
   // Frontend
   'webapp/src/**/*.{ts,tsx,js}': [
     'eslint --cache --cache-location ./webapp/.eslintcache --fix',
-    'prettier --loglevel warn --cache --write',
+    'prettier --log-level warn --cache --write',
   ],
 
-  'webapp/**/*.{json,mjs,yml,scss}': 'prettier --loglevel warn --cache --write',
+  'webapp/**/*.{json,mjs,yml,scss}': 'prettier --log-level warn --cache --write',
 
   // Backend
   'backend/src/**/*.{ts,tsx,js}': [
     'eslint --cache --cache-location ./backend/.eslintcache --fix',
-    'prettier --loglevel warn --cache --write',
+    'prettier --log-level warn --cache --write',
   ],
 
-  'backend/**/*.{json,mjs,yml,scss}': 'prettier --loglevel warn --cache --write',
+  'backend/**/*.{json,mjs,yml,scss}': 'prettier --log-level warn --cache --write',
 
   // Корневые конфиги, утилиты, скрипты
-  '*.{ts,js,mjs,json,yml}': 'prettier --loglevel warn --cache --write',
+  '*.{ts,js,mjs,json,yml}': 'prettier --log-level warn --cache --write',
 
   '**/*.scss': 'stylelint --cache --cache-location ./node_modules/.cache/stylelintcache --fix',
 }

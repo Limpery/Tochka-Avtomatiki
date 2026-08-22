d0c2c962952b44f6f3d2d80412b31f5887553cda
Boxomol <turnaev20@mail.ru>
2026-08-22 19:34:14 +0400

    Add form succes message

--- webapp/src/pages/NewMemPage/index.tsx
+++ webapp/src/pages/NewMemPage/index.tsx
@@ -5,8 +5,10 @@ import { Input } from '../../components/Input'
 import { Textarea } from '../../components/Textarea'
 import { withZodSchema } from 'formik-validator-zod'
 import { trpc } from '../../lib/trpc'
+import { useState } from 'react'
 
 export const NewMemPage = () => {
+  const [succesMessageVisible, setSuccessMessageVisible] = useState(false)
   const createMem = trpc.createMem.useMutation()
   const formik = useFormik({
     initialValues: {
@@ -18,6 +20,11 @@ export const NewMemPage = () => {
     validate: withZodSchema(zCreateMemTrpcInput),
     onSubmit: async (values) => {
       await createMem.mutateAsync(values)
+      formik.resetForm()
+      setSuccessMessageVisible(true)
+      setTimeout(() => {
+        setSuccessMessageVisible(false)
+      }, 3000)
     },
   })
 
@@ -34,7 +41,7 @@ export const NewMemPage = () => {
         <Input name="description" label="Description" formik={formik} />
         <Textarea name="text" label="Text" formik={formik} />
         {!formik.isValid && !!formik.submitCount && <div style={{ color: 'red' }}>Some fields are invalid</div>}
-
+        {succesMessageVisible && <div style={{ color: 'green' }}>Mem created</div>}
         <button type="submit" disabled={formik.isSubmitting}>
           {formik.isSubmitting ? 'Submitting...' : 'Create Mem'}
         </button>

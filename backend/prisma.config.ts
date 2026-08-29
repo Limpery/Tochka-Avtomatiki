897e863255589b903b598231c19a2d5799bce49d
Boxomol <turnaev20@mail.ru>
2026-08-29 20:46:36 +0400

    Add prisma config

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'prisma/config'
 export default defineConfig({
   schema: './src/prisma/schema.prisma',
   datasource: {
-    url: process.env.DATABASE_URL,
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    url: process.env.DATABASE_URL!,
   },
 })
